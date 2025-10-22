af0a8dfb0978e0f94e6e95e17bf1d7a1892eefe8
Toad169 <mamangpunten893@gmail.com>
2025-10-22 13:23:57 +0700

    added alpine, htmx and tailwindcss ofc

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'htmx.org'
+
+import './main.css'
+
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
+
 const app = createApp(App)
 
 app.use(createPinia())
